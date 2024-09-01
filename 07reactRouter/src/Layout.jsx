cae0abf81fcb8c84853c5b3959774231c23f727a
theanikettales <theanikettales@gmail.com>
2024-09-01 08:02:08 +0530

    react projects

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/Header/Header.'
+import Header from "./components/Header/Header.jsx";
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
